dc4f18694b3b20939fcc16a0062d64245e1cb2a3
Tommie Terhoeve <tommieterhoeve@gmail.com>
2018-03-29 14:18:27 +0200

    Server Socket works

--- scripts/centralsystem/server.py
+++ scripts/centralsystem/server.py
@@ -1,16 +1,33 @@
 #!/usr/bin/python
 import socket
 
-s = socket.socket()
-host = socket.gethostbyname(socket.gethostname())
 
-print ('Local IP address: ', host)
-port = 34567
+class Server:
 
-s.bind((host, port))
-s.listen(5)
-while True:
-    c, addr = s.accept()
-    print ('Connected ', addr)
-    c.send('Thanks bruv')
-    c.close()
+    def __init__(self):
+        # Create a TCP/IP socket
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Get local server IP
+        host = socket.gethostbyname(socket.gethostname())
+        port = 34567
+        print('Local IP address: ', host)
+        self.s.bind((host, port))
+        # 5 sockets max
+        self.s.listen(5)
+
+    def connect_socket(self):
+        # Wait for client to connect to server
+        c, addr = self.s.accept()
+        print('Connected ', addr)
+        c.send(b'Ground control to major Tom')
+        # Receive client message
+        received = c.recv(4096)
+        print(received)
+        # Close connection with client
+        c.close()
+        print('Closed connection with ', addr)
+
+
+serversocket = Server()
+
+serversocket.connect_socket()
