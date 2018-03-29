57d6ae0a1add796a9e16ede3c9594bb8e4a7d126
Tommie Terhoeve <tommieterhoeve@gmail.com>
2018-03-29 12:58:53 +0200

    test

---
+++ scripts/robot/server.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import socket
+
+s = socket.socket()
+host = socket.gethostbyname(socket.gethostname())
+
+print ('Local IP address: 'host)
+port = 34567
+
+s.bind((host, port))
+s.listen(5)
+while True:
+    c, addr = s.accept()
+    print ('Connected ', addr)
+    c.send('Thanks bruv')
+    c.close()
