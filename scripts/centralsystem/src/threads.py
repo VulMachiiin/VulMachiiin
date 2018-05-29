eaabaa449138cf1c8f39c37179c9c7461487bd21
Ghoris0127 <0935462@hr.nl>
2018-05-29 11:16:47 +0200

    the great server reformation of 2k18 can start

--- scripts/centralsystem/src/threads.py
+++ scripts/centralsystem/src/threads.py
@@ -19,7 +19,7 @@ class RobotConnection(threading.Thread):
             json_message = self.receive()
             if json_message['type'] == 'ready':
                 if json_message['value'] == True:
-                    self.        
+                    self.server.server_processes.robot_ready = True        
             elif json_message['type'] == 'arrived_at_target':
 
             elif json_message['type'] == 'unload':
@@ -49,11 +49,14 @@ class RobotConnection(threading.Thread):
 
 class DatabaseHook(threading.Thread):
 
-    def __init__(self, db_connection, server_processes):
-        self.db_connection = db_connection
+    def __init__(self, server_processes):
         self.server_processes = server_processes
 
     def run(self):
         while True:
-            self.db_connection.get_query('SELECT product_id, shelve_id, x_coordinate, y_coordinate FROM productsinshelve WHERE amount_in_cartridge < 0')
-            
+            tray_values = self.server_processes.db_connector.get_query('SELECT product_id, shelve_id, x_coordinate, y_coordinate FROM productsinshelve WHERE amount_in_cartridge < 0')
+            #check if items in this
+            for item in tray_values:
+                self.server_processes.trays_to_place.append(item)
+            sleep(5) #change this delay to set update speed
+                
