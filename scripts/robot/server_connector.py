e2bfa38f2accba25ba50d6ab4e60f6a8a4023360
Tommie Terhoeve <tommieterhoeve@gmail.com>
2018-06-16 17:40:34 +0200

    Altered boot.py

--- scripts/robot/server_connector.py
+++ scripts/robot/server_connector.py
@@ -5,21 +5,21 @@ import Encryptor
 import IO_controller
 class Server_Connector(threading.Thread):
 
-
     encryptor = Encryptor()
     iocontroller = IO_controller()
 
-    def __init__(self):
+    def __init__(self, IP_address, port):
         print('Creating socket')
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.IP_address = IP_address
+        self.port = port
         #TODO ip en port global
         #ciphertext = self.s.recv(4096)
         #decryptedmessage = self.do_decrypt(ciphertext))
-        # Save me from the darks\
 
-    def run(self, IP_address, port):
-        self.s.connect((IP_address, port))
-        print('Succesfully connected to: ', IP_address)
+    def run(self):
+        self.s.connect((self.IP_address, self.port))
+        print('Succesfully connected to: ', self.IP_address)
         # Send a 1 to the server to let it know that you're a robot
         self.send('1')
         message = self.s.receive(1024)
@@ -31,11 +31,13 @@ class Server_Connector(threading.Thread):
                 directions = message['routes']
                 for directionlist in directions:
                     for direction in directionlist:
+                        print('Next direction: ' + direction)
                         while(self.iocontroller.detect_node() == "line"):
                             pass
                         if(self.iocontroller.detect_node() == "node"):
                             # TODO draai naar dirction
-                            self.iocontroller.control_motors(directions)
+                            print("Node detected")
+                            #self.iocontroller.control_motors(directions)
 
     def send(self, message):
         jsonmessage = json.dumps(message)
